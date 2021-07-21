08b176ce16cc901d2ada5dcad06e42cbf0eefdae
Davi <davi.carvalho@interligados.com.br>
2021-07-21 14:06:23 -0300

    first commit

---
+++ src/test/java/com/davimc/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davimc.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
