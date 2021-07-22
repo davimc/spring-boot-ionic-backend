cbca0dc5788c6646d0b4e860ffe174aa2ee506ae
Davi <davi.carvalho@interligados.com.br>
2021-07-22 11:23:07 -0300

    Corrigindo teste

--- src/main/java/com/davimc/cursomc/CursomcApplication.java
+++ src/main/java/com/davimc/cursomc/CursomcApplication.java
@@ -30,18 +30,15 @@ public class CursomcApplication implements CommandLineRunner {
 		Produto p2 = new Produto("Impressora", 800.00);
 		Produto p3 = new Produto("Mouse", 80.00);
 
-		cat1.setProdutos(Arrays.asList(p1,p2,p3));
-		cat2.setProdutos(Arrays.asList(p2));
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
 
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
-
-
-
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 
 
 	}
