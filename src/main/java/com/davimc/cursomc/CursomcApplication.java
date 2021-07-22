5178affab41011f3fb7684abac4078cf0baa46e4
Davi <davi.carvalho@interligados.com.br>
2021-07-22 11:16:10 -0300

    testando categoria e produto

--- src/main/java/com/davimc/cursomc/CursomcApplication.java
+++ src/main/java/com/davimc/cursomc/CursomcApplication.java
@@ -1,13 +1,48 @@
 package com.davimc.cursomc;
 
+import com.davimc.cursomc.domain.Categoria;
+import com.davimc.cursomc.domain.Produto;
+import com.davimc.cursomc.repositories.CategoriaRepository;
+import com.davimc.cursomc.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import java.util.Arrays;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria("Informática");
+		Categoria cat2 = new Categoria("Escritório");
+
+		Produto p1 = new Produto("Computador", 2000.00);
+		Produto p2 = new Produto("Impressora", 800.00);
+		Produto p3 = new Produto("Mouse", 80.00);
+
+		cat1.setProdutos(Arrays.asList(p1,p2,p3));
+		cat2.setProdutos(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+
+
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+
+
+	}
 }
