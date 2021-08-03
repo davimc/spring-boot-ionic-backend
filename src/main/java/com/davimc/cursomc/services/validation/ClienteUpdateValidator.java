72c0c282726e3520932b504ee66f9edb901bf3e9
Davi <davimatosc@hotmail.com>
2021-08-02 22:57:52 -0300

    Validacao customizada: email repetido na atualizacao de Cliente

---
+++ src/main/java/com/davimc/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,50 @@
+package com.davimc.cursomc.services.validation;
+
+import com.davimc.cursomc.domain.Cliente;
+import com.davimc.cursomc.dto.ClienteDTO;
+import com.davimc.cursomc.dto.ClienteNewDTO;
+import com.davimc.cursomc.enums.TipoCliente;
+import com.davimc.cursomc.repositories.ClienteRepository;
+import com.davimc.cursomc.resources.exceptions.FieldMessage;
+import com.davimc.cursomc.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+    @Autowired
+    private ClienteRepository repo;
+    @Autowired
+    private HttpServletRequest request;
+
+    @Override
+    public void initialize(ClienteUpdate ann) {
+    }
+    @Override
+    public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+
+        Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        Long uriId = Long.parseLong(map.get("id"));
+        List<FieldMessage> list = new ArrayList<>();
+
+        // inclua os testes aqui, inserindo erros na lista
+        Cliente aux = repo.findByEmail(objDto.getEmail()).get();
+        if(aux != null && !aux.getId().equals(uriId)) {
+
+        }
+            list.add(new FieldMessage("email", "E-mail já cadastrado"));
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage())
+                    .addPropertyNode(e.getFieldName()).addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
