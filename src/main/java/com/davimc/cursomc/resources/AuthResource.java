e0d3ce9816463f01b668ad0e95d78e94674bbbaa
Davi <davimatosc@hotmail.com>
2021-09-01 21:45:42 -0300

    Refresh token

---
+++ src/main/java/com/davimc/cursomc/resources/AuthResource.java
@@ -0,0 +1,29 @@
+package com.davimc.cursomc.resources;
+
+import com.davimc.cursomc.security.JWTUtil;
+import com.davimc.cursomc.security.UserSS;
+import com.davimc.cursomc.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthResource {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @PostMapping("/refresh_token")
+    public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+        UserSS user = UserService.authenticated();
+        String token = jwtUtil.generateToken(user.getUsername());
+        response.addHeader("Authorization", "Bearer " + token);
+        return ResponseEntity.noContent().build();
+
+    }
+}
